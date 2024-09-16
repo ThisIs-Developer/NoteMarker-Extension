08a8b6dc8b0868290bd0bb01d4254c8d154a6456
Baivab Sarkar <baivabsarkar@gmail.com>
2024-09-16 21:16:48 +0530

    Update highlighter

--- background.js
+++ background.js
@@ -1,21 +1,20 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("Webpage Annotator installed.");
-  });
-  
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "saveAnnotations") {
+  console.log("Webpage Annotator installed.");
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "saveAnnotations") {
       chrome.storage.local.set({ annotations: message.data }, () => {
-        console.log("Annotations saved.");
-        sendResponse({ status: "success" });
+          console.log("Annotations saved.");
+          sendResponse({ status: "success" });
       });
       return true;
-    }
-  
-    if (message.action === "getAnnotations") {
+  }
+
+  if (message.action === "getAnnotations") {
       chrome.storage.local.get(["annotations"], (result) => {
-        sendResponse({ annotations: result.annotations || [] });
+          sendResponse({ annotations: result.annotations || [] });
       });
       return true;
-    }
-  });
-  
+  }
+});
